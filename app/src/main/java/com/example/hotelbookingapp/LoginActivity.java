bc135fcc10c19c6aa89f22cc5872ae7a73d43d21
Amandeepsuggal <42380227+Amandeepsuggal@users.noreply.github.com>
2020-02-25 15:27:54 -0500

    .......

--- app/src/main/java/com/example/hotelbookingapp/LoginActivity.java
+++ app/src/main/java/com/example/hotelbookingapp/LoginActivity.java
@@ -23,49 +23,19 @@ public class LoginActivity extends AppCompatActivity {
 
 
         public class Main2Activity extends AppCompatActivity {
-            TextView t11;
             Button b3, b4, b5;
-            ConstraintLayout l1;
-            String name, password, email, savedemail;
-            SharedPreferences sp;
-            SharedPreferences.Editor editor;
-            EditText ed1, ed2;
-            Intent i;
-            View v;
+           Intent i;
 
             @Override
             protected void onCreate(Bundle savedInstanceState) {
                 super.onCreate(savedInstanceState);
                 setContentView(R.layout.activity_login);
-                ed1 = findViewById(R.id.editText2);
-                ed2 = findViewById(R.id.editText3);
+
                 b3 = findViewById(R.id.button11);
                 b4 = findViewById(R.id.button3);
                 b5 = findViewById(R.id.button5);
 
 
-
-
-                sp = getSharedPreferences(Signupactivity.MyPREFERENCES, MODE_PRIVATE);
-
-
-                b3.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        name = ed1.getText().toString();
-                        password = ed1.getText().toString();
-
-                        email = ed2.getText().toString();
-                        editor = sp.edit();
-                        editor.putString("key1", name);
-
-                        editor.putString("key3", email);
-                        editor.apply();
-
-
-
-                    }
-                });
 
                 b4.setOnClickListener(new View.OnClickListener() {
                     @Override
--- app/src/main/java/com/example/hotelbookingapp/ReviewActivity.java
+++ app/src/main/java/com/example/hotelbookingapp/ReviewActivity.java
@@ -2,13 +2,25 @@ package com.example.hotelbookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReviewActivity extends AppCompatActivity {
-
+Button bb1,bb2,bb3,bb4;
+Intent i;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
+        bb1 = findViewById(R.id.button2);
+        bb1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(ReviewActivity.this, SearchHotelActivity.class);
+            }
+        });
     }
 }
